8d36737f4d9df1358fc142de639511979899e701
FlowerGolden <1327097302@qq.com>
2024-03-21 15:55:08 +0800

    feat: impl base PQueue

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+new PQueue<int>(((x, y) => x - y), new List<int>() { 6, 4, 8, 7, 9 });
+
+class PQueue<T>
+{
+    private readonly Func<T, T, int> _comparison;
+    private List<T> _list;
+
+    public PQueue(Func<T, T, int> comparison, List<T> list)
+    {
+        this._comparison = comparison;
+        this._list = list;
+        _list.Sort(Compare);
+    }
+
+    private int Compare(T a, T b)
+    {
+        return _comparison(a, b);
+    }
+}
