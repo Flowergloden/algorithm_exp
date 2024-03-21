1c0850b8923887c4989a01185286cc124da91a66
FlowerGolden <1327097302@qq.com>
2024-03-21 19:53:51 +0800

    feat: impl dnc

--- Program.cs
+++ Program.cs
@@ -5,37 +5,48 @@ var sol = new Sol();
 var lhs = new List<int> { 1, 3, 5, 7, 9 };
 var rhs = new List<int> { 2, 4, 6, 8, 10 };
 var nth = 5;
-var result = sol.Solve(lhs, rhs, 5);
+var result = sol.Solve(lhs, rhs, nth);
+Console.WriteLine(result);
 
 public class Sol
 {
-    public T Solve<T>(IList<T> lhs, IList<T> rhs, int nth, int leftIndex = 0, int rightIndex = 0)
+    public T Solve<T>(IList<T> lhs, IList<T> rhs, int nth) // nth is the ACTUAL index
         where T : IComparable<T>
     {
-        if (leftIndex == 0 && rightIndex == 0)
+        if (lhs.Count == 0)
         {
-            leftIndex = lhs.Count;
-            rightIndex = rhs.Count;
+            return rhs[nth];
         }
 
-        if (leftIndex + rightIndex + 2 == nth)
-            return lhs[leftIndex].CompareTo(rhs[rightIndex]) >= 0 ? lhs[leftIndex] : rhs[rightIndex];
-
-        if (leftIndex + rightIndex + 2 > nth)
+        if (rhs.Count == 0)
         {
-            leftIndex /= 2;
-            rightIndex /= 2;
-            return Solve(lhs, rhs, leftIndex, rightIndex, nth);
+            return lhs[nth];
         }
 
-        if (leftIndex + rightIndex < nth)
-        {
-            leftIndex += leftIndex / 2;
-            rightIndex += rightIndex / 2;
-            return Solve(lhs, rhs, leftIndex, rightIndex, nth);
-        }
-        // TODO: find proper way to divide
+        var lhsMid = lhs.Count / 2;
+        var rhsMid = rhs.Count / 2;
 
-        throw new Exception("error");
+        if (lhsMid + rhsMid < nth)
+        {
+            if (lhs[lhsMid].CompareTo(rhs[rhsMid]) < 0)
+            {
+                return Solve(lhs.Skip(lhsMid + 1).ToList(), rhs, nth - lhsMid - 1);
+            }
+            else
+            {
+                return Solve(lhs, rhs.Skip(rhsMid + 1).ToList(), nth - rhsMid - 1);
+            }
+        }
+        else
+        {
+            if (lhs[lhsMid].CompareTo(rhs[rhsMid]) < 0)
+            {
+                return Solve(lhs, rhs.Take(rhsMid).ToList(), nth);
+            }
+            else
+            {
+                return Solve(lhs.Take(lhsMid).ToList(), rhs, nth);
+            }
+        }
     }
 }
