a25f96cba65e724d2df21b50fe4147d2a877dcc7
FlowerGolden <1327097302@qq.com>
2024-03-21 16:29:22 +0800

    feat: add more method

--- Program.cs
+++ Program.cs
@@ -1,18 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-new PQueue<int>(((x, y) => x - y), new List<int>() { 6, 4, 8, 7, 9 });
 
 class PQueue<T>
 {
-    private readonly Func<T, T, int> _comparison;
-    private List<T> _list;
+    private readonly Func<T, T, int>? _comparison;
+    private readonly List<T> _list;
+    private readonly bool _bDefaultComparison;
+    public int Count => _list.Count;
 
     public PQueue(Func<T, T, int> comparison, List<T> list)
     {
-        this._comparison = comparison;
-        this._list = list;
+        _comparison = comparison;
+        _list = list;
+        _bDefaultComparison = false;
         _list.Sort(Compare);
+    }
+
+    public PQueue(List<T> list)
+    {
+        _list = list;
+        _bDefaultComparison = true;
+        _list.Sort();
+    }
+
+    public PQueue()
+    {
+        _list = new();
+        _bDefaultComparison = true;
+        _list.Sort();
+    }
+
+    public void Enqueue(T item)
+    {
+        _list.Add(item);
+        if (_bDefaultComparison)
+        {
+            _list.Sort();
+            return;
+        }
+        _list.Sort(Compare);
+    }
+
+    public T Dequeue()
+    {
+        var tmp = _list[0];
+        _list.RemoveAt(0);
+        return tmp;
     }
 
     private int Compare(T a, T b)
