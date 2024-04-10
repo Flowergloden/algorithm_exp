a4a4e5266c2d5107deba674214a196f3af2285a1
FlowerGolden <1327097302@qq.com>
2024-04-10 16:25:11 +0800

    feat: impl DP

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-var items = new List<(int, int)> { (20, 10), (30, 20), (65, 30), (40, 40), (60, 50) };
-var maxWeight = 100;
+var items = new List<(int, int)> { (10, 1), (15, 2), (40, 3) };
+var maxWeight = 5;
 Console.WriteLine(Solution.Greedy(items, maxWeight));
+Console.WriteLine(Solution.DynamicProgramming(items, maxWeight));
 
 public static class Solution
 {
@@ -25,6 +26,39 @@ public static class Solution
 
             res += item.Item1;
         }
+
+        return res;
+    }
+
+    public static int DynamicProgramming(List<(int, int)> items, int maxWeight) // (value, weight)
+    {
+        var dp = new List<List<int>>();
+        for (int i = 0; i <= items.Count; i++)
+        {
+            var lst = new List<int>();
+            for (int j = 0; j <= maxWeight; j++)
+            {
+                lst.Add(0);
+            }
+
+            dp.Add(lst);
+        }
+
+        for (int i = 1; i < dp.Count; i++)
+        {
+            for (int j = dp[i].Count - 1; j > i; j--)
+            {
+                if (items[i - 1].Item2 > j)
+                {
+                    dp[i][j] = dp[i - 1][j];
+                    continue;
+                }
+
+                dp[i][j] = int.Max(dp[i - 1][j], dp[i - 1][j - items[i - 1].Item2] + items[i - 1].Item1);
+            }
+        }
+
+        var res = dp.Select(x => x.Max()).Max();
 
         return res;
     }
