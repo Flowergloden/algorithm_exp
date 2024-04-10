f83cadaf905e05485ca4a545e9fdf7391bc3be70
FlowerGolden <1327097302@qq.com>
2024-04-10 15:40:47 +0800

    feat: impl greedy algorithm

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+var items = new List<(double, double)> { (20, 10), (30, 20), (65, 30), (40, 40), (60,50) };
+var maxWeight = 100;
+Console.WriteLine(Solution.Greedy(items, maxWeight));
+
+public static class Solution
+{
+    public static double Greedy(List<(double, double)> items, double maxWeight) // (value, weight)
+    {
+        items.Sort((a, b) => (b.Item1 / b.Item2 - a.Item1 / a.Item2) < 0 ? -1 : 1);
+        double res = 0;
+        double weight = 0;
+        foreach (var item in items)
+        {
+            weight += item.Item2;
+
+            if (weight > maxWeight)
+            {
+                weight -= item.Item2;
+                res += (maxWeight - weight) * item.Item1 / item.Item2;
+                break;
+            }
+
+            res += item.Item1;
+        }
+        return res;
+    }
+}
